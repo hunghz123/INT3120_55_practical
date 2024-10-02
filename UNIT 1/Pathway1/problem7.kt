8785979dc5422f8652b0a5264de5b208e5d0d33c
hunghz123 <133178295+hunghz123@users.noreply.github.com>
2024-10-03 03:28:52 +0700

    Add files via upload

---
+++ UNIT 1/Pathway1/problem7.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = subtract(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber - $thirdNumber = $anotherResult")
+}
+
+// Define add() function below this line
+fun add(a: Int, b: Int): Int{
+    return a + b;
+}
+
+fun subtract(a: Int, b: Int): Int{
+    return a - b;
+}
---
+++ UNIT 1/Pathway1/problem9.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val Steps = 4000
+    val caloriesBurned = pedometerStepsToCalories(Steps);
+    println("Walking $Steps steps burns $caloriesBurned calories")
+}
+
+fun pedometerStepsToCalories(numberOfSteps: Int): Double {
+    val caloriesBurnedForEachStep = 0.04
+    val totalCaloriesBurned = numberOfSteps * caloriesBurnedForEachStep
+    return totalCaloriesBurned
+}
