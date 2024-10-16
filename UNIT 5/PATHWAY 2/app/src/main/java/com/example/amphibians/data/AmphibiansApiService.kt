5b212cbb598b47d38513668fb6ed6052bda10c37
hunghz123 <hunghz123@gmail.com>
2024-10-17 05:52:30 +0700

    UNIT 5 PATHWAY 2

---
+++ UNIT 5/PATHWAY 2/app/src/main/java/com/example/amphibians/data/AmphibiansApiService.kt
@@ -0,0 +1,9 @@
+package com.example.amphibians.data
+
+import com.example.amphibians.model.Amphibian
+import retrofit2.http.GET
+
+interface AmphibiansApiService {
+    @GET("amphibians")
+    suspend fun getAmphibians(): List<Amphibian>
+}
